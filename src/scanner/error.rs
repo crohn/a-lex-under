550bf50e8a336ae57007bb830b150bc5dbb4ced1
Davide Giuseppe Monaco <monaco.dg@gmail.com>
2025-04-29 23:40:05 +0200

    refactor: ParseError implements Error and Display

--- src/scanner/error.rs
+++ src/scanner/error.rs
@@ -1,4 +1,15 @@
+use std::error::Error;
+use std::fmt::{self, Display};
+
 #[derive(Debug, PartialEq)]
 pub struct ParseError {
     pub message: String,
+}
+
+impl Error for ParseError {}
+
+impl Display for ParseError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.message)
+    }
 }
