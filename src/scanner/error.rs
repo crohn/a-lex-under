f52ee59ba985cc5def0c8c67fa629ee545fb9d56
Davide Giuseppe Monaco <monaco.dg@gmail.com>
2025-05-10 07:45:41 +0200

    refactor: grouped common write call in error Display impl

--- src/scanner/error.rs
+++ src/scanner/error.rs
@@ -37,86 +37,73 @@ impl Error for ScanError {}
 
 impl Display for ScanError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "error@{},{}: ", self.row, self.col)?;
+
         match self.kind {
-            ScanErrorKind::InvalidCodepoint(codepoint) => write!(
-                f,
-                "error@{},{}: invalid UTF-8 codepoint '{:x}'",
-                self.row, self.col, codepoint
-            ),
-            ScanErrorKind::InvalidHexDigit(c) => write!(
-                f,
-                "error@{},{}: unexpected character '{}'. Invalid hex digit.",
-                self.row, self.col, c
-            ),
+            ScanErrorKind::InvalidCodepoint(codepoint) => {
+                write!(f, "invalid UTF-8 codepoint '{:x}'", codepoint)
+            }
+            ScanErrorKind::InvalidHexDigit(c) => {
+                write!(f, "unexpected character '{}'. Invalid hex digit.", c)
+            }
             ScanErrorKind::InvalidIdentifier(c) => write!(
                 f,
-                "error@{},{}: unexpected character '{}'. Identifier can only contain alphanumeric or '_' characters.",
-                self.row, self.col, c
+                "unexpected character '{}'. Identifier can only contain alphanumeric or '_' characters.",
+                c
             ),
             ScanErrorKind::InvalidLiteralEscape(c) => write!(
                 f,
-                "error@{},{}: unexpected character '{}'. The sequence '\\{}' is not valid, only '\\n', '\\r', '\\t' are supported.",
-                self.row, self.col, c, c
+                "unexpected character '{}'. The sequence '\\{}' is not valid, only '\\n', '\\r', '\\t' are supported.",
+                c, c
             ),
             ScanErrorKind::InvalidLongOption(c) => write!(
                 f,
-                "error@{},{}: unexpected character '{}'. Long option can only contain alphanumeric or '_', '-', '=' characters.",
-                self.row, self.col, c
+                "unexpected character '{}'. Long option can only contain alphanumeric or '_', '-', '=' characters.",
+                c
             ),
             ScanErrorKind::InvalidLongOptionSequence(p, c) => write!(
                 f,
-                "error@{},{}: unexpected character '{}'. Invalid '{}{}' sequence in long option.",
-                self.row, self.col, c, p, c
+                "unexpected character '{}'. Invalid '{}{}' sequence in long option.",
+                c, p, c
             ),
             ScanErrorKind::InvalidLongOptionStart(c) => write!(
                 f,
-                "error@{},{}: unexpected character '{}'. Long option starting character must be alphanumeric.",
-                self.row, self.col, c
+                "unexpected character '{}'. Long option starting character must be alphanumeric.",
+                c
             ),
             ScanErrorKind::InvalidTokenStart(c) => write!(
                 f,
-                "error@{},{}: unexpected character '{}'. Token starting character must be alphabetic, \" or -.",
-                self.row, self.col, c
+                "unexpected character '{}'. Token starting character must be alphabetic, \" or -.",
+                c
             ),
             ScanErrorKind::UnbalancedCurlyBracket => {
-                write!(
-                    f,
-                    "error@{},{}: unbalanced closing curly bracket '}}'",
-                    self.row, self.col
-                )
+                write!(f, "unbalanced closing curly bracket '}}'")
             }
-            ScanErrorKind::UnbalancedDoubleQuotes => write!(
-                f,
-                "error@{},{}: unbalanced closing quote '\"'.",
-                self.row, self.col
-            ),
+            ScanErrorKind::UnbalancedDoubleQuotes => {
+                write!(f, "unbalanced closing quote '\"'.")
+            }
             ScanErrorKind::UnexpectedControlCharacter(c) => write!(
                 f,
-                "error@{},{}: unexpected control character '{}'. Only '\\n', '\\r', '\\t' are supported.",
-                self.row,
-                self.col,
+                "unexpected control character '{}'. Only '\\n', '\\r', '\\t' are supported.",
                 c.escape_unicode()
             ),
             ScanErrorKind::UnexpectedOptionContinuation(c) => write!(
                 f,
-                "error@{},{}: unexpected character '{}'. Expected one alphanumeric or '-' to continue to Short or Long option.",
-                self.row, self.col, c
+                "unexpected character '{}'. Expected one alphanumeric or '-' to continue to Short or Long option.",
+                c
             ),
-            ScanErrorKind::UnexpectedOptionEndOfInput => write!(
-                f,
-                "error@{},{}: unexpected end of input after '-' character.",
-                self.row, self.col
-            ),
+            ScanErrorKind::UnexpectedOptionEndOfInput => {
+                write!(f, "unexpected end of input after '-' character.",)
+            }
             ScanErrorKind::UnexpectedShortOptionContinuation(c) => write!(
                 f,
-                "error@{},{}: unexpected character '{}'. Short option can contain only one UTF-8 alphanumeric character.",
-                self.row, self.col, c
+                "unexpected character '{}'. Short option can contain only one UTF-8 alphanumeric character.",
+                c
             ),
             ScanErrorKind::UnexpectedUnicodeOpeningCurlyBracket => {
                 write!(
                     f,
-                    "error@{},{}: unexpected character '{{'. Expected codepoint hex digit.",
-                    self.row, self.col
+                    "unexpected character '{{'. Expected codepoint hex digit."
                 )
             }
         }
