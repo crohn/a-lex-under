cb7b6e52507a5d0a2a5e0c37a92fca4ce16adbc7
Davide Giuseppe Monaco <monaco.dg@gmail.com>
2025-05-17 17:00:12 +0200

    feat: tokenizer (option-long)

--- src/tokenizer.rs
+++ src/tokenizer.rs
@@ -2,6 +2,7 @@ use crate::cursor::Cursor;
 use crate::scanner::Scanner;
 use std::mem;
 
+const EQUALS: char = '=';
 const HYPHEN: char = '-';
 
 #[derive(Debug, PartialEq)]
@@ -11,6 +12,7 @@ pub enum State {
     EmitTokenWhitespace,
     End,
     Identifier,
+    OptionLong,
     OptionSelect,
     Whitespace,
 }
@@ -18,7 +20,9 @@ pub enum State {
 #[derive(Debug, PartialEq)]
 pub enum Token {
     Identifier(String),
+    Operator(String),
     OptionShort(String),
+    OptionLong(String),
     Whitespace,
 }
 
@@ -52,6 +56,7 @@ impl<'a> Tokenizer<'a> {
         match self.state {
             State::Begin => self.handle_begin(),
             State::Identifier => self.handle_identifier(),
+            State::OptionLong => self.handle_option_long(),
             State::OptionSelect => self.handle_option_select(),
             State::Whitespace => self.handle_whitespace(),
             _ => unreachable!("<{:?}> {:?}", self.state, self.cursor),
@@ -60,6 +65,11 @@ impl<'a> Tokenizer<'a> {
 
     fn handle_begin(&mut self) -> Result<State, TokenizationError> {
         match (self.cursor.curr, self.cursor.next) {
+            (Some(EQUALS), None) => Err(TokenizationError::UnexpectedEndOfInput),
+            (Some(EQUALS), _) => {
+                self.string_buffer.push(EQUALS);
+                Ok(State::EmitTokenString(Token::Operator))
+            }
             (Some(HYPHEN), None) => Err(TokenizationError::UnexpectedEndOfInput),
             (Some(HYPHEN), _) => Ok(State::OptionSelect),
             (Some(c), None) if c.is_whitespace() => Ok(State::EmitTokenWhitespace),
@@ -104,9 +114,33 @@ impl<'a> Tokenizer<'a> {
         }
     }
 
+    fn handle_option_long(&mut self) -> Result<State, TokenizationError> {
+        // NOTE curr can't be None, because of lookahead in handle_option_select
+        match (self.cursor.curr, self.cursor.next) {
+            (Some(c), _) if c.is_whitespace() => Err(TokenizationError::UnexpectedWhitespace),
+            (Some(c), None) => {
+                self.string_buffer.push(c);
+                Ok(State::EmitTokenString(Token::OptionLong))
+            },
+            (Some(c), Some(k)) => {
+                self.string_buffer.push(c);
+                if k.is_whitespace() {
+                    Ok(State::EmitTokenString(Token::OptionLong))
+                } else if k == EQUALS {
+                    Ok(State::EmitTokenString(Token::OptionLong))
+                } else {
+                    Ok(State::OptionLong)
+                }
+            },
+            _ => unreachable!("<{:?}> {:?}", self.state, self.cursor),
+        }
+    }
+
     fn handle_option_select(&mut self) -> Result<State, TokenizationError> {
         // NOTE curr can't be None, because of lookahead in handle_begin
         match (self.cursor.curr, self.cursor.next) {
+            (Some(HYPHEN), None) => Err(TokenizationError::UnexpectedEndOfInput),
+            (Some(HYPHEN), _) => Ok(State::OptionLong),
             (Some(c), _) if c.is_whitespace() => Err(TokenizationError::UnexpectedWhitespace),
             (Some(c), _) => {
                 self.string_buffer.push(c);
@@ -243,6 +277,49 @@ mod test {
                 Ok(Token::Whitespace),
                 Ok(Token::OptionShort("o".to_string())),
                 Ok(Token::Identifier("4".to_string())),
+            ]
+        );
+
+        let tokenizer = Tokenizer::new(Scanner::new("\t f --o4 7"));
+        let tokens: Vec<Result<Token, TokenizationError>> = tokenizer.collect();
+        assert_eq!(
+            tokens,
+            vec![
+                Ok(Token::Whitespace),
+                Ok(Token::Identifier("f".to_string())),
+                Ok(Token::Whitespace),
+                Ok(Token::OptionLong("o4".to_string())),
+                Ok(Token::Whitespace),
+                Ok(Token::Identifier("7".to_string())),
+            ]
+        );
+
+        let tokenizer = Tokenizer::new(Scanner::new("\t f --o4=7"));
+        let tokens: Vec<Result<Token, TokenizationError>> = tokenizer.collect();
+        assert_eq!(
+            tokens,
+            vec![
+                Ok(Token::Whitespace),
+                Ok(Token::Identifier("f".to_string())),
+                Ok(Token::Whitespace),
+                Ok(Token::OptionLong("o4".to_string())),
+                Ok(Token::Operator("=".to_string())),
+                Ok(Token::Identifier("7".to_string())),
+            ]
+        );
+
+        let tokenizer = Tokenizer::new(Scanner::new("\t f --o4= 7"));
+        let tokens: Vec<Result<Token, TokenizationError>> = tokenizer.collect();
+        assert_eq!(
+            tokens,
+            vec![
+                Ok(Token::Whitespace),
+                Ok(Token::Identifier("f".to_string())),
+                Ok(Token::Whitespace),
+                Ok(Token::OptionLong("o4".to_string())),
+                Ok(Token::Operator("=".to_string())),
+                Ok(Token::Whitespace),
+                Ok(Token::Identifier("7".to_string())),
             ]
         );
     }
